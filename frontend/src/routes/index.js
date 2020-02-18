adc4f804fc52d859336fd0fe3f94c4b01394f802
Silas P. Ladislau <splhead@gmail.com>
2020-02-17 20:43:20 -0400

    frontend started

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Routes';
+
+import SignIn from '../pages/SignIn';
+import Delivery from '../pages/Delivery';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/delivery" component={Delivery} isPrivate />
+    </Switch>
+  );
+}
